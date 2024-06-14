f1372e081c7c5e7bb8d2386d0abda7020adcc8c7
Jburlama <jhonas.burlamaqui@gmail.com>
2024-06-14 17:58:25 +0100

    redirects with no here_doc

--- srcs/rumredir.c
+++ srcs/rumredir.c
@@ -6,7 +6,7 @@
 /*   By: Jburlama <Jburlama@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 16:14:16 by Jburlama          #+#    #+#             */
-/*   Updated: 2024/06/14 17:31:45 by Jburlama         ###   ########.fr       */
+/*   Updated: 2024/06/14 17:57:39 by Jburlama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,24 @@
 
 void	rumredir(void *root, t_data *data)
 {
-	int		out;
+	int		fd;
 
 	if (((t_redir *)root)->file_type == O)
 	{
-		out = open(((t_redir *)root)->file, O_CREAT | O_TRUNC | O_RDWR, S_IRWXU);
-		dup2(out, STDOUT_FILENO);
+		fd = open(((t_redir *)root)->file, O_CREAT | O_TRUNC | O_RDWR,
+			 S_IRWXU);
+		dup2(fd, STDOUT_FILENO);
+	}
+	else if (((t_redir *)root)->file_type == APEND)
+	{
+		fd = open(((t_redir *)root)->file, O_CREAT | O_APPEND | O_RDWR,
+			 S_IRWXU);
+		dup2(fd, STDOUT_FILENO);
+	}
+	else if (((t_redir *)root)->file_type == I)
+	{
+		fd = open(((t_redir *)root)->file, O_CREAT | O_RDWR, S_IRWXU);
+		dup2(fd, STDIN_FILENO);
 	}
 	rumcmd(((t_redir *)root)->down, data);
 }
